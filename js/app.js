96b2ce7c60b1630eb72f91bb34864b66b6471bb6
jelson <jelson20@outlook.com>
2020-07-07 22:14:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,3 +12,5 @@ let functionOne = multplerFunction(2)
 let functionTwo = multplerFunction(3)
 let functionThree = multplerFunction(4)
 let functionFour = multplerFunction(5)
+
+document.querySelector('#app').innerHTML = functionTwo;
