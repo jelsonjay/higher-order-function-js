35aa72a171bedc5a9712a9689d882e5cf7c8c214
jelson <jelson20@outlook.com>
2020-07-07 22:08:31 +0100

    Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function multplerFunction(multpler) {
+	return function (e) {
+    return e * 5 multpler
+    };
+}
+
+
+//
+let functionOne = multplerFunction(2)
+let functionTwo = multplerFunction(3)
+let functionThree = multplerFunction(4)
+let functionFour = multplerFunction(5)
