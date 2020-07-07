331a0e666129e23e453a8a4ee78416b42b493475
jelson <jelson20@outlook.com>
2020-07-07 22:24:13 +0100

    Commit

--- js/app.js
+++ js/app.js
@@ -2,15 +2,14 @@
 
 function multplerFunction(multpler) {
 	return function (e) {
-    return e * 5 multpler
-    };
+		return e * multpler;
+	};
 }
 
-
 //
-let functionOne = multplerFunction(2)
-let functionTwo = multplerFunction(3)
-let functionThree = multplerFunction(4)
-let functionFour = multplerFunction(5)
+let functionOne = multplerFunction(2);
+let functionTwo = multplerFunction(3);
+let functionThree = multplerFunction(4);
+let functionFour = multplerFunction(5);
 
-document.querySelector('#app').innerHTML = functionTwo;
+document.querySelector('#app').innerHTML = functionTwo(2);
